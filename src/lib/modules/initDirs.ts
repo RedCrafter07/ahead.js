1fc3fb8e5e884bd8a24f572db29bf028cec87af8
RedCrafter07 <gh@r07.dev>
2023-03-28 17:03:24 +0200

    Util function for creating multiple directories.

---
+++ src/lib/modules/initDirs.ts
@@ -0,0 +1,10 @@
+import { existsSync } from 'fs';
+import { mkdir } from 'fs/promises';
+
+export default async function initDirs(paths: string[]) {
+	for (const path of paths) await initDir(path);
+}
+
+async function initDir(path: string) {
+	if (!(await existsSync(path))) await mkdir(path, { recursive: true });
+}
