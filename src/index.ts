0b664df987f08802fd4499b5530ac65a6dc92cb4
RedCrafter07 <gh@r07.dev>
2023-03-28 17:02:16 +0200

    An index file (temporarily for testing)

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import genRoutes from './lib/genRoutes';
+import init from './lib/init';
+
+const cwd = process.cwd();
+const aheadDir = path.join(cwd, '.ahead');
+
+(async () => {
+	await init(cwd);
+
+	console.log('Initialized!');
+
+	const routes = await genRoutes(cwd);
+
+	await writeFile(
+		path.join(aheadDir, 'routes.json'),
+		JSON.stringify(routes, null, 2),
+	);
+})();
