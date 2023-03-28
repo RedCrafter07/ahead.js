30cf4e01a48f94dd63a1166360def46fcff20d36
RedCrafter07 <gh@r07.dev>
2023-03-28 17:03:40 +0200

    Function to get all file names from a directory

---
+++ src/lib/modules/readContents.ts
@@ -0,0 +1,22 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+
+export default async function readContents(dir: string) {
+	const paths: string[] = [];
+
+	async function getContents(dir: string) {
+		const content = await readdir(dir, { withFileTypes: true });
+
+		for (const item of content) {
+			if (item.isDirectory()) {
+				await getContents(path.join(dir, item.name));
+			} else {
+				paths.push(path.join(dir, item.name));
+			}
+		}
+	}
+
+	await getContents(dir);
+
+	return paths;
+}
