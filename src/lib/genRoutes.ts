f7dc2a9d5c77b914e3215374fd035f23e29caa1c
RedCrafter07 <gh@r07.dev>
2023-03-29 14:20:52 +0200

    Add router generator (thanks Fishi)

--- src/lib/genRoutes.ts
+++ src/lib/genRoutes.ts
@@ -1,8 +1,9 @@
 import path from 'path';
 import readContents from './util/readContents';
+import { RouteObject } from 'react-router-dom';
 
-export default async function genRoutes(cwd: string) {
-	const routeDir = path.join(cwd, 'pages');
+export default async function getRoutes(cwd: string) {
+	const routeDir = path.join(cwd, '.ahead', 'build', 'pre', 'client', 'routes');
 	const contents = (await readContents(routeDir))
 		.map((p) => ({
 			fileLocation: p,
@@ -25,7 +26,70 @@ export default async function genRoutes(cwd: string) {
 		.map((p) => ({
 			...p,
 			path: p.path.length > 1 ? p.path.replace(/\/$/, '') : p.path,
+		}))
+		.map((p) => ({
+			...p,
+			path: p.path.replace(path.sep, '/'),
 		}));
 
 	return contents;
+}
+
+interface Route {
+	path: string;
+	children: (Route | { index: true; element: string })[];
+}
+
+interface IndexedRoute {
+	path: string;
+	fileLocation: string;
+}
+
+export async function genRouterData(indexed: IndexedRoute[]): Promise<Route[]> {
+	const router: Route[] = [];
+
+	for (const i of indexed) {
+		const path = i.path.split('/');
+		let current: Route = { path: '', children: router };
+
+		let subpath = path[1];
+		if (subpath.startsWith('{') && subpath.endsWith('}'))
+			subpath = ':' + subpath.substring(1, subpath.length - 1);
+		subpath = '/' + subpath;
+
+		const newCurrent = current.children.find(
+			(el) => !(el as any).index && (el as any).path === subpath,
+		);
+		if (!newCurrent) {
+			const newPath: Route = {
+				path: subpath,
+				children: [],
+			};
+			current.children.push(newPath);
+			current = newPath;
+		} else current = newCurrent as Route;
+
+		for (let subpath of path.slice(2)) {
+			if (subpath.startsWith('{') && subpath.endsWith('}'))
+				subpath = ':' + subpath.substring(1, subpath.length - 1);
+
+			const newCurrent = current.children.find(
+				(el) => !(el as any).index && (el as any).path === subpath,
+			);
+			if (!newCurrent) {
+				const newPath: Route = {
+					path: subpath,
+					children: [],
+				};
+				current.children.push(newPath);
+				current = newPath;
+			} else current = newCurrent as Route;
+		}
+		current.children.push({
+			index: true,
+			element: i.fileLocation,
+		});
+	}
+
+	return router;
 }
