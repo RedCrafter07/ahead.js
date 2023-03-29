480462e5e803116610b07162a695c8bdeebe24f8
RedCrafter07 <gh@r07.dev>
2023-03-29 14:20:24 +0200

    Add default index file

---
+++ src/lib/client/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Router from './router';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<Router />);
