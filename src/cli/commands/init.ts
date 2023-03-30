2c3d9c2296ca0290ce23e917769e98c73fd0823f
RedCrafter07 <gh@r07.dev>
2023-03-30 17:28:11 +0200

    Add init command

---
+++ src/cli/commands/init.ts
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import init from '../../lib/init';
+
+export default {
+	name: 'init',
+	aliases: ['i'],
+	description: 'Initialize a new Ahead.js project',
+	options: [],
+	async exec() {
+		console.log(
+			chalk.hex('#009BFF')('[Ahead.js]'),
+			chalk.yellow('Initializing folders...'),
+		);
+
+		await init(process.cwd());
+
+		console.log(
+			chalk.hex('#009BFF')('[Ahead.js]'),
+			chalk.green('Done! Have fun! 🚀'),
+		);
+	},
+} as Command;
