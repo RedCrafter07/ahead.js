058f11784d3c20936017f95da945c7611727f72d
RedCrafter07 <gh@r07.dev>
2023-03-30 13:21:21 +0200

    Change initialization

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -1,24 +1,33 @@
 import { existsSync } from 'fs';
 import { symlink } from 'fs/promises';
 import path from 'path';
-import initDirs from './util/initDirs';
+import initDirs, { initDir } from './util/initDirs';
+
+const neededDirs = [
+	['.ahead', 'build', 'dist'],
+	['.ahead', 'config'],
+	['.ahead', 'build', 'pre', 'client'],
+	['.ahead', 'build', 'pre', 'server'],
+];
 
 export default async function init(cwd: string) {
-	if (await existsSync(path.join(cwd, '.ahead'))) return;
-
-	await initDirs(
-		[
-			['.ahead', 'build', 'dist'],
-			['.ahead', 'config'],
-			['.ahead', 'build', 'pre', 'client'],
-			['.ahead', 'build', 'pre', 'server'],
-		].map((p) => path.join(cwd, ...p)),
+	await Promise.all(
+		neededDirs
+			.map((p) => path.join(path.join(cwd, ...p)))
+			.map(async (d) => {
+				await initDir(d);
+			}),
 	);
 
-	await createSymlink(
-		path.join(cwd, 'pages'),
-		path.join(cwd, '.ahead', 'build', 'pre', 'client', 'routes'),
-	);
+	if (
+		!(await existsSync(
+			path.join(cwd, '.ahead', 'build', 'pre', 'client', 'routes'),
+		))
+	)
+		await createSymlink(
+			path.join(cwd, 'pages'),
+			path.join(cwd, '.ahead', 'build', 'pre', 'client', 'routes'),
+		);
 }
 
 async function createSymlink(source: string, target: string) {
