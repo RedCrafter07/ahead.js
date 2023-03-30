50f672162a8b5c52f9517a78c90bf7c86ee8ba42
RedCrafter07 <gh@r07.dev>
2023-03-30 17:28:17 +0200

    Add a command parser

---
+++ src/cli/index.ts
@@ -0,0 +1,35 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+
+export const commands: { [key: string]: Command } = {};
+
+(async () => {
+	const commandDirFiles = await readdir(path.join(__dirname, 'commands'), {
+		withFileTypes: true,
+	});
+	const commandFiles = commandDirFiles.filter(
+		(f) => f.isFile() && (f.name.endsWith('.js') || f.name.endsWith('.ts')),
+	);
+
+	for (const file of commandFiles) {
+		const { default: command } = await import(`./commands/${file.name}`);
+		commands[command.name] = command;
+	}
+
+	const cmd = process.argv.slice(2)[0];
+
+	const run = Object.values(commands).find(
+		(c) => c.name === cmd || c.aliases.includes(cmd),
+	);
+
+	if (!cmd) return await commands.help.exec();
+
+	if (!run) {
+		console.log(
+			'Invalid command! Please use "ahead help" to see a list of commands.',
+		);
+		return;
+	}
+
+	await run.exec();
+})();
