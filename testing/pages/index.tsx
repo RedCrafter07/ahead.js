523f54633b66cf16ba28eb1e6f0648655ed66d85
RedCrafter07 <gh@r07.dev>
2023-03-30 13:06:37 +0200

    Add test things to test SSR

--- testing/pages/index.tsx
+++ testing/pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <>This is a test</>;
+	return (
+		<>
+			This is a test
+			<button onClick={() => alert('You clicked me!')}>Click me!</button>
+		</>
+	);
 }
