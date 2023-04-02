51d88df50639147115eccfdc624a7fd1afa2da57
RedCrafter07 <gh@r07.dev>
2023-04-02 17:24:40 +0200

    Some changes
    - Create pages dir
    - Add checkDirs function

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -2,12 +2,14 @@ import { existsSync } from 'fs';
 import { symlink } from 'fs/promises';
 import path from 'path';
 import initDirs, { initDir } from './util/initDirs';
+import chalk from 'chalk';
 
 const neededDirs = [
 	['.ahead', 'build', 'dist'],
 	['.ahead', 'config'],
 	['.ahead', 'build', 'pre', 'client'],
 	['.ahead', 'build', 'pre', 'server'],
+	['pages'],
 ];
 
 export default async function init(cwd: string) {
@@ -32,4 +34,21 @@ export default async function init(cwd: string) {
 
 async function createSymlink(source: string, target: string) {
 	await symlink(source, target, 'junction');
+}
+
+export async function checkDirs(cwd: string) {
+	const dirsExist = await Promise.all(
+		neededDirs.map(async (p) => ({
+			exists: await existsSync(path.join(path.join(cwd, ...p))),
+			path: path.join(path.join(cwd, ...p)),
+		})),
+	);
+
+	// check if a directory doesn't exist
+	if (dirsExist.filter((p) => p.exists == false).length > 0)
+		throw new Error(
+			chalk.red(
+				"The needed directories for Ahead.js don't seem to exist. Please run 'ahead init' to create them or check if you're in the correct directory.",
+			),
+		);
 }
