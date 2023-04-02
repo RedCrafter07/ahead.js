976cb9da89c0c7265eeddba21f3911c5793913fd
RedCrafter07 <gh@r07.dev>
2023-04-02 16:27:03 +0200

    Change type

--- src/cli/types/command.ts
+++ src/cli/types/command.ts
@@ -2,12 +2,13 @@ interface Command {
 	name: string;
 	description: string;
 	aliases: string[];
-	options: CommandOption[];
+	options?: CommandOption[];
+	syntax?: string;
 	exec: () => Promise<void>;
 }
 
 interface CommandOption {
 	name: string;
 	description: string;
-	aliases: string[];
+	default: string;
 }
