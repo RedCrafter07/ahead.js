5db1e7bd404439082d6f7920c4454c97867ba57f
RedCrafter07 <gh@r07.dev>
2023-04-02 19:50:17 +0200

    Update server compiler

--- src/lib/compilers/server.ts
+++ src/lib/compilers/server.ts
@@ -1,48 +1,52 @@
 import path from 'path';
-import { webpack } from 'webpack';
+import { Configuration, webpack } from 'webpack';
 import AheadLoggingPlugin from './plugins/logging';
+import ServerStarterPlugin from './plugins/serverStarter';
+import PreServerPlugin from './plugins/pre/server';
 
-export default async function compileServer(dir: string) {
+export default async function compileServer(
+	dir: string,
+	mode: Configuration['mode'] = 'production',
+) {
 	dir = path.resolve(dir);
 
-	const compiler = webpack({
-		mode: 'development',
-		entry: {
-			server: path.join(dir, 'pre', 'server', 'index.tsx'),
-		},
-		output: {
-			path: `${dir}/dist/server`,
-			filename: '[name].js',
-		},
-		target: 'node',
-		resolve: {
-			extensions: ['.ts', '.tsx', '.js', '.jsx'],
-		},
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					loader: 'esbuild-loader',
-					options: {
-						loader: 'tsx',
-						target: 'esnext',
+	const compiler = webpack(
+		{
+			mode,
+			entry: {
+				server: path.join(dir, 'pre', 'server', 'index.tsx'),
+			},
+			output: {
+				path: `${dir}/dist/server`,
+				filename: '[name].js',
+			},
+			target: 'node',
+			resolve: {
+				extensions: ['.ts', '.tsx', '.js', '.jsx'],
+			},
+			module: {
+				rules: [
+					{
+						test: /\.tsx?$/,
+						loader: 'esbuild-loader',
+						options: {
+							loader: 'tsx',
+							target: 'esnext',
+						},
+						exclude: /node_modules/,
 					},
-					exclude: /node_modules/,
-				},
+				],
+			},
+			plugins: [
+				new PreServerPlugin(),
+				new AheadLoggingPlugin('server'),
+				new ServerStarterPlugin(),
 			],
+			optimization: {
+				minimize: false,
+			},
+			watch: mode === 'development',
 		},
-		plugins: [new AheadLoggingPlugin('server')],
-	});
-
-	await new Promise((resolve, reject) => {
-		compiler.run((err, stats) => {
-			if (err) {
-				reject(err);
-			} else if (stats?.hasErrors()) {
-				reject(stats.toString());
-			} else {
-				resolve(stats);
-			}
-		});
-	});
+		(err, stats) => {},
+	);
 }
