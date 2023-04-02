6c8d8bb015cddbe3faee2884e60438de9bd6d67f
RedCrafter07 <gh@r07.dev>
2023-04-02 19:49:52 +0200

    Update some stuff

--- src/lib/compilers/client.ts
+++ src/lib/compilers/client.ts
@@ -1,90 +1,89 @@
 import path from 'path';
-import { webpack } from 'webpack';
+import { webpack, Configuration } from 'webpack';
 import HTMLPlugin from 'html-webpack-plugin';
 import AheadLoggingPlugin from './plugins/logging';
+import PreClientPlugin from './plugins/pre/client';
 
-export default async function compileClient(dir: string, htmlTemplate: string) {
-	const compiler = webpack({
-		mode: 'development',
-		entry: {
-			client: path.join(dir, 'pre', 'client', 'index.tsx'),
-		},
-		output: {
-			path: `${dir}/dist/client`,
-			filename: '[name].js',
-			publicPath: '/.ahead/',
-		},
-		optimization: {
-			splitChunks: {
-				chunks: 'all',
+export default async function compileClient(
+	dir: string,
+	htmlTemplate: string,
+	mode: Configuration['mode'] = 'production',
+) {
+	const compiler = webpack(
+		{
+			mode,
+			entry: {
+				client: path.join(dir, 'pre', 'client', 'index.tsx'),
 			},
-		},
-		resolve: {
-			extensions: [
-				'.js',
-				'.ts',
-				'.jsx',
-				'.tsx',
-				'.css',
-				'.png',
-				'.svg',
-				'.ttf',
-				'.gif',
-				'.webp',
-				'.jpg',
-				'.jpeg',
+			output: {
+				path: `${dir}/dist/client`,
+				filename: '[name].js',
+				publicPath: '/.ahead/',
+			},
+			optimization: {
+				splitChunks: {
+					chunks: 'all',
+				},
+				minimize: false,
+			},
+			resolve: {
+				extensions: [
+					'.js',
+					'.ts',
+					'.jsx',
+					'.tsx',
+					'.css',
+					'.png',
+					'.svg',
+					'.ttf',
+					'.gif',
+					'.webp',
+					'.jpg',
+					'.jpeg',
+				],
+			},
+			plugins: [
+				new PreClientPlugin(),
+				new HTMLPlugin({
+					template: htmlTemplate,
+				}),
+				new AheadLoggingPlugin('client'),
 			],
-		},
-		plugins: [
-			new HTMLPlugin({
-				template: htmlTemplate,
-			}),
-			new AheadLoggingPlugin('client'),
-		],
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					use: [
-						{ loader: 'style-loader' },
-						{ loader: 'css-loader' },
-						{ loader: 'postcss-loader' },
-					],
-				},
-				{
-					test: /\.s[ca]ss$/,
-					use: [
-						{ loader: 'style-loader' },
-						{ loader: 'css-loader' },
-						{ loader: 'sass-loader' },
-					],
-				},
-				{
-					test: /\.tsx?$/,
-					loader: 'esbuild-loader',
-					options: {
-						loader: 'tsx',
-						target: 'esnext',
+			module: {
+				rules: [
+					{
+						test: /\.css$/,
+						use: [
+							{ loader: 'style-loader' },
+							{ loader: 'css-loader' },
+							{ loader: 'postcss-loader' },
+						],
 					},
-				},
-				{
-					test: /\.(png|ttf|svg|gif|webp|jpg|jpeg)$/, // to import images and fonts
-					loader: 'url-loader',
-					options: { limit: false },
-				},
-			],
+					{
+						test: /\.s[ca]ss$/,
+						use: [
+							{ loader: 'style-loader' },
+							{ loader: 'css-loader' },
+							{ loader: 'sass-loader' },
+						],
+					},
+					{
+						test: /\.tsx?$/,
+						loader: 'esbuild-loader',
+						options: {
+							loader: 'tsx',
+							target: 'esnext',
+						},
+					},
+					{
+						test: /\.(png|ttf|svg|gif|webp|jpg|jpeg)$/, // to import images and fonts
+						loader: 'url-loader',
+						options: { limit: false },
+					},
+				],
+			},
+			watch: mode === 'development',
 		},
-	});
-
-	await new Promise((resolve, reject) => {
-		compiler.run((err, stats) => {
-			if (err) {
-				reject(err);
-			} else if (stats?.hasErrors()) {
-				reject(stats.toString());
-			} else {
-				resolve(stats);
-			}
-		});
-	});
+		(err, stats) => {},
+	);
 }
