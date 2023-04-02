e881789505f2786e660f7cf1d0fbf91192ad7430
RedCrafter07 <gh@r07.dev>
2023-04-02 16:25:58 +0200

    Add build command

---
+++ src/cli/commands/build.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import build from '../../build';
+
+const cwd = process.cwd();
+export const aheadDir = path.join(cwd, '.ahead');
+
+export default {
+	name: 'build',
+	aliases: [],
+	description: 'Build the Ahead.js app',
+	async exec() {
+		await build();
+	},
+} as Command;
