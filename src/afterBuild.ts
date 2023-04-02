263a4fbfd3a9b4a698586589d0188dcd5ed82c3a
RedCrafter07 <gh@r07.dev>
2023-04-02 23:26:42 +0200

    Create afterBuild.ts
    Copying files not copied by typescript

---
+++ src/afterBuild.ts
@@ -0,0 +1,43 @@
+import { copyFile, mkdir, readdir } from 'fs/promises';
+import path from 'path';
+
+(async () => {
+	async function copyFiles(dir: string) {
+		const files = await readdir(dir, { withFileTypes: true });
+		await Promise.all(
+			files.map(async (file) => {
+				if (file.isDirectory()) {
+					await copyFiles(path.join(dir, file.name));
+				}
+
+				if (
+					file.isFile() &&
+					(file.name.endsWith('.txt') || file.name.endsWith('.html'))
+				) {
+					console.log(`Copying ${file.name} to build folder`);
+
+					await mkdir(
+						path.join(
+							path.join(__dirname, '..', 'dist'),
+							dir.slice(__dirname.length),
+						),
+						{
+							recursive: true,
+						},
+					);
+
+					await copyFile(
+						path.join(dir, file.name),
+						path.join(
+							path.join(__dirname, '..', 'dist'),
+							dir.slice(__dirname.length),
+							file.name,
+						),
+					);
+				}
+			}),
+		);
+	}
+
+	await copyFiles(__dirname);
+})();
