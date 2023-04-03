42d33c96e93054ec961bb776039c7c632cc4b76e
RedCrafter07 <gh@r07.dev>
2023-04-03 22:03:49 +0200

    Change credits page

--- testing/src/pages/credits.tsx
+++ testing/src/pages/credits.tsx
@@ -1,8 +1,7 @@
 export default function Credits() {
 	return (
 		<>
-			<p>This is a test!</p>
-			<button onClick={() => alert('You clicked me!')}>A button!</button>
+			<p>Credits page</p>
 		</>
 	);
 }
