503456fd26cd25c5ad4c1196e59e7fce5ec1d9c3
RedCrafter07 <gh@r07.dev>
2023-04-03 20:10:15 +0200

    Add 3000 as optional port
    If no env is there, the port will be set to 3000 instead of undefined.

--- src/lib/routeGeneration/server.ts
+++ src/lib/routeGeneration/server.ts
@@ -26,6 +26,6 @@ async function sendClient(req: express.Request, res: express.Response) {
 
 ${serverRouter}
 
-app.listen(process.env.AHEAD_PORT, () => {console.log("Listening on port " + process.env.AHEAD_PORT)})
+app.listen(process.env.AHEAD_PORT ?? 3000, () => {console.log("Listening on port " + process.env.AHEAD_PORT)})
 `;
 }
