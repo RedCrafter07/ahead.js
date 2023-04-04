cfc02ed8084d82369da7669653a1c21b06dabb1e
RedCrafter07 <gh@r07.dev>
2023-04-04 20:03:11 +0200

    Limit the exports

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,15 @@
-export * from 'react-router-dom';
+export {
+	useLocation,
+	useNavigate,
+	useFetcher,
+	useFetchers,
+	useActionData,
+	useFormAction,
+	useHref,
+	Link,
+	LinkProps,
+	Form,
+	FormProps,
+	ScrollRestoration,
+	ScrollRestorationProps,
+} from 'react-router-dom';
