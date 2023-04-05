afa2792247bfcc27ae3848836d9c777994015b40
RedCrafter07 <gh@r07.dev>
2023-04-05 23:36:08 +0200

    Update test preload for new syntax

--- testing/src/preload/test.ts
+++ testing/src/preload/test.ts
@@ -1,11 +1,11 @@
 import { Preload } from 'ahead.js';
 
-const preload: Preload = (app) => {
+const preload: Preload = ({ app, server }) => {
 	app.get('/noteboards', (req, res) => {
 		res.send('Hello there!');
 	});
 
-	return app;
+	return { app, server };
 };
 
 export default preload;
