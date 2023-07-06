237bc412a3fcbe84b1b32796eb0b7682daf28e8e
RedCrafter07 <gh@r07.dev>
2023-07-06 18:45:41 +0200

    Optimize dev server
    Start seperate compilations for client and server changes instead of compiling everything no matter what

--- src/devServer.ts
+++ src/devServer.ts
@@ -1,13 +1,19 @@
 import chalk from 'chalk';
 import { FSWatcher, watch } from 'fs';
 import path from 'path';
-import { buildAll } from './build';
+import { buildAll, buildClient, buildServer } from './build';
 import { ChildProcess, spawn } from 'child_process';
+import chokidar from 'chokidar';
+import compileClient from './lib/compilers/client';
 
 class DevServer {
+	// the directory to watch
 	dir: string;
-	watcher?: FSWatcher;
 	port: string;
+
+	// watchers
+	clientWatcher?: chokidar.FSWatcher;
+	serverWatcher?: chokidar.FSWatcher;
 
 	serverProcess?: ChildProcess;
 
@@ -31,7 +37,21 @@ class DevServer {
 			'Watching for changes...',
 		);
 
-		this.watcher = watch(this.dir, { recursive: true });
+		this.clientWatcher = chokidar.watch(
+			path.join(this.dir, 'pages').replace(path.sep, '/'),
+			{
+				persistent: true,
+			},
+		);
+
+		this.serverWatcher = chokidar.watch(
+			['preload', 'server'].map((p) =>
+				path.join(this.dir, p).replace(path.sep, '/'),
+			),
+			{
+				persistent: true,
+			},
+		);
 
 		this.startProcess();
 
@@ -39,22 +59,42 @@ class DevServer {
 	}
 
 	registerEvents() {
-		this.watcher?.once('change', async (eventType) => {
-			console.log(
-				chalk.hex('#0099ff').bold('[ahead]'),
-				chalk.hex('#0E7AF3')('File changed! Rebuilding...'),
-			);
+		this.registerClient();
+		this.registerServer();
+	}
 
-			await this.handleChange();
+	registerServer() {
+		this.serverWatcher?.once('change', async () => {
+			await this.handleServer();
 		});
 	}
 
-	async handleChange() {
+	registerClient() {
+		this.clientWatcher?.on('change', async () => {
+			await this.handleClient();
+		});
+	}
+
+	async handleClient() {
+		console.log(
+			chalk.hex('#0099ff').bold('[ahead]'),
+			'Client changed! Rebuilding...',
+		);
+		const buildTime = await buildClient('development');
+
+		console.log(
+			chalk.hex('#0099ff').bold('[ahead]'),
+			`Rebuilt in ${buildTime}ms`,
+		);
+	}
+
+	async handleServer() {
+		console.log(chalk.hex('#0099ff').bold('[ahead]'), 'Server changed!');
 		console.log(chalk.hex('#0099ff').bold('[ahead]'), 'Stopping server...');
 		this.serverProcess?.kill();
 
 		console.log(chalk.hex('#0099ff').bold('[ahead]'), 'Rebuilding...');
-		const buildTime = await buildAll('development');
+		const buildTime = await buildServer('development');
 
 		console.log(
 			chalk.hex('#0099ff').bold('[ahead]'),
@@ -96,7 +136,8 @@ class DevServer {
 	async stop() {
 		console.log('Closing dev server...');
 		this.serverProcess?.kill();
-		this.watcher?.close();
+		this.clientWatcher?.close();
+		this.serverWatcher?.close();
 	}
 }
 
