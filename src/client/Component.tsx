e682c711809e3d6b93b33e671bdd3743bca5013d
RedCrafter07 <gh@r07.dev>
2023-07-07 14:11:05 +0200

    Add component typing

--- src/client/Component.tsx
+++ src/client/Component.tsx
@@ -3,3 +3,5 @@ import { ReactNode } from 'react';
 export default function Component(props: { children: ReactNode }) {
 	return <>{props.children}</>;
 }
+
+export type Component = typeof Component;
