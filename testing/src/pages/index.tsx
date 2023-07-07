052504bdb0b06b0a1351d043944f1ebf1810f5f2
RedCrafter07 <gh@r07.dev>
2023-07-07 13:56:42 +0200

    Add tailwind styling and navigate

--- testing/src/pages/index.tsx
+++ testing/src/pages/index.tsx
@@ -1,12 +1,23 @@
 import './index.css';
+import { useNavigate } from 'react-router-dom';
+import { startTransition } from 'react';
 
 // @ahead/title Home
 
 export default function Home() {
+	const navigate = useNavigate();
+
 	return (
-		<>
-			<p>This is a test!</p>
-			<button onClick={() => alert('You clicked me!')}>A button!</button>
-		</>
+		<div className='w-full min-h-screen bg-zinc-700 text-white'>
+			<div className='p-2 container mx-auto flex flex-col gap-2'>
+				<p className='text-3xl'>This is a test!</p>
+				<button
+					onClick={() => startTransition(() => navigate('/test'))}
+					className='p-2 bg-green-600 rounded-lg text-white hover:opacity-90 active:scale-95 transition-all duration-75'
+				>
+					A button!
+				</button>
+			</div>
+		</div>
 	);
 }
