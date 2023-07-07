77821544edb80314d3122ac293ece41da964a4bf
RedCrafter07 <gh@r07.dev>
2023-07-07 12:03:15 +0200

    Check for title in routes

--- src/lib/routeGeneration/getRoutes.ts
+++ src/lib/routeGeneration/getRoutes.ts
@@ -1,13 +1,39 @@
 import path from 'path';
 import readContents from '../util/readContents';
+import { readFile } from 'fs/promises';
 
 export default async function getRoutes(cwd: string) {
 	const routeDir = path.join(cwd, '.ahead', 'build', 'pre', 'client', 'routes');
-	const contents = (await readContents(routeDir))
-		.map((p) => ({
-			fileLocation: p,
-			path: p,
-		}))
+	const contents = (
+		await Promise.all(
+			(
+				await readContents(routeDir)
+			)
+				.map((p) => ({
+					fileLocation: p,
+					path: p,
+				}))
+				.map(async (p) => {
+					const fileContent = await readFile(p.fileLocation, 'utf-8');
+
+					let title: string | undefined;
+
+					const titleRegex = /(\/\/ ?@ahead\/title ).+/g;
+
+					const lines = fileContent
+						.split('\n')
+						.filter((l) => l.match(titleRegex));
+
+					if (lines.length > 0)
+						title = lines[0].replace(/\/\/ ?@ahead\/title /g, '').trim();
+
+					return {
+						...p,
+						title,
+					};
+				}),
+		)
+	)
 		// remove the routeDir and the file extension
 		.map((p) => ({
 			...p,
