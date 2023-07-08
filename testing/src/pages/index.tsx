c9e12f83d19b12f620c3f7eb15ea414d0befa734
RedCrafter07 <gh@r07.dev>
2023-07-08 21:23:06 +0200

    Remove page specific styles

--- testing/src/pages/index.tsx
+++ testing/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	const navigate = useNavigate();
 
 	return (
-		<div className='w-full min-h-screen bg-zinc-700 text-white'>
+		<div>
 			<title>Home</title>
 			<div className='p-2 container mx-auto flex flex-col gap-2'>
 				<p className='text-3xl'>This is a test!</p>
