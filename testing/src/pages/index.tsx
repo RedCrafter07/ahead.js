7a3085ba3d5c77447788f2e629f28ffa46fd2db5
RedCrafter07 <gh@r07.dev>
2023-07-08 20:23:17 +0200

    Add home title

--- testing/src/pages/index.tsx
+++ testing/src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { startTransition } from 'react';
 
-// @ahead/title Home
-
 export default function Home() {
 	const navigate = useNavigate();
 
 	return (
 		<div className='w-full min-h-screen bg-zinc-700 text-white'>
+			<title>Home</title>
 			<div className='p-2 container mx-auto flex flex-col gap-2'>
 				<p className='text-3xl'>This is a test!</p>
 				<button
