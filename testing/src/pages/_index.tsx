26d1b1da71dafe67600879f8f94a445e98e24372
RedCrafter07 <gh@r07.dev>
2023-07-08 21:13:13 +0200

    Add new logic & page transition

--- testing/src/pages/_index.tsx
+++ testing/src/pages/_index.tsx
@@ -1,8 +1,27 @@
 import './index.css';
-import { Index } from 'ahead.js/client';
+import { Index, useLocation } from 'ahead.js/client';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const MyApp: Index = ({ content }) => {
-	return <>{content}</>;
+const MyApp: Index = ({ Router, props }) => {
+	const location = useLocation();
+
+	return (
+		<div>
+			<AnimatePresence mode='wait' initial={false}>
+				<motion.div
+					key={location.pathname}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{
+						duration: 0.2,
+					}}
+				>
+					<Router {...props} />
+				</motion.div>
+			</AnimatePresence>
+		</div>
+	);
 };
 
 export default MyApp;
