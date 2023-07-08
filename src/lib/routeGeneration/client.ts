449638d9f84e021b0e6cfdb35513d84fda31af5d
RedCrafter07 <gh@r07.dev>
2023-07-08 16:24:33 +0200

    Add route component generation

--- src/lib/routeGeneration/client.ts
+++ src/lib/routeGeneration/client.ts
@@ -2,8 +2,10 @@ import path from 'path';
 import { aheadDir } from '../../build';
 
 interface Route {
-	path: string;
-	children: (Route | { index: true; element: string })[];
+	index?: boolean;
+	path?: string;
+	children?: Route[];
+	element?: string;
 }
 
 export interface IndexedRoute {
@@ -22,7 +24,7 @@ function transformImports(data: IndexedRoute[]): {
 		if (i > -1) d.fileLocation = '<Import_' + i + ' />';
 		else {
 			imports.push(d.fileLocation);
-			d.fileLocation = '<Import_' + (imports.length - 1) + ' />';
+			d.fileLocation = `<Import_${imports.length - 1} />`;
 		}
 	}
 
@@ -37,6 +39,8 @@ function transformToRoutes(indexed: IndexedRoute[]): Route[] {
 	for (const i of indexed) {
 		const path = i.path.split('/');
 		let current: Route = { path: '', children: router };
+
+		if (!current.children) current.children = [];
 
 		let subpath = path[1];
 		if (subpath.startsWith('{') && subpath.endsWith('}'))
@@ -59,6 +63,8 @@ function transformToRoutes(indexed: IndexedRoute[]): Route[] {
 			if (subpath.startsWith('{') && subpath.endsWith('}'))
 				subpath = ':' + subpath.substring(1, subpath.length - 1);
 
+			if (!current.children) current.children = [];
+
 			const newCurrent = current.children.find(
 				(el) => !(el as any).index && (el as any).path === subpath,
 			);
@@ -71,6 +77,9 @@ function transformToRoutes(indexed: IndexedRoute[]): Route[] {
 				current = newPath;
 			} else current = newCurrent as Route;
 		}
+
+		if (!current.children) current.children = [];
+
 		current.children.push({
 			index: true,
 			element: i.fileLocation,
@@ -78,6 +87,36 @@ function transformToRoutes(indexed: IndexedRoute[]): Route[] {
 	}
 
 	return router;
+}
+
+function transformToComponents(routes: Route[]) {
+	function generateComponents(routes: Route[]) {
+		let routeString = '';
+
+		for (const route of routes) {
+			if (
+				(route.path == '/' || route.path == '') &&
+				route.children &&
+				route.children.length > 0
+			) {
+				routeString += generateComponents(route.children);
+			} else if (route.children && route.children.length > 0) {
+				routeString += `<Route path="${route.path}">\n`;
+				routeString += generateComponents(route.children);
+				routeString += `</Route>\n`;
+			} else {
+				routeString += `<Route ${
+					route.path ? `path="${route.path}"` : 'index'
+				} element={${route.element}}/>`;
+			}
+		}
+
+		return routeString;
+	}
+
+	return `<>
+${generateComponents(routes)}
+</>`;
 }
 
 export function transform(routes: IndexedRoute[], client: boolean = true) {
@@ -103,6 +142,41 @@ export function transform(routes: IndexedRoute[], client: boolean = true) {
 	}
 	file += '\n\nexport default ';
 	file += JSON.stringify(router, null, 2);
+	// file += transformToComponents(router);
+
+	return file.replaceAll(
+		/"element":\s*"(<Import_[0-9]+ \/>)"/g,
+		'"element": $1',
+	); // turn "<Element_1 />" to <Element_1>
+}
+
+export function transformClient(
+	routes: IndexedRoute[],
+	client: boolean = true,
+) {
+	let file = '';
+
+	const { data, imports } = transformImports(routes);
+	const router = transformToRoutes(data);
+
+	for (let i = 0; i < imports.length; ++i) {
+		const importData = imports[i]
+			.split('.')
+			.slice(0, -1)
+			.join('.')
+			.replaceAll(path.sep, '/')
+			.slice(path.join(aheadDir, 'build', 'pre', 'client').length);
+
+		const jsonImport = JSON.stringify(
+			`.${!client ? './client' : ''}${importData}`,
+		);
+		if (client)
+			file += `const Import_${i} = React.lazy(() => import(${jsonImport}));\n`;
+		else file += `import Import_${i} from ${jsonImport};\n`;
+	}
+	file += '\n\nexport default ';
+	// file += JSON.stringify(router, null, 2);
+	file += transformToComponents(router);
 
 	return file.replaceAll(
 		/"element":\s*"(<Import_[0-9]+ \/>)"/g,
