90faff846b52a9ccdfd71c476811e791b4c74f2b
RedCrafter07 <gh@r07.dev>
2023-07-08 19:02:43 +0200

    Replace AHEAD_ENV with the environment

--- src/lib/compilers/plugins/pre/client.ts
+++ src/lib/compilers/plugins/pre/client.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 import path from 'path';
 import { aheadDir, root } from '../../../../paths';
 import { transformClient as transform } from '../../../routeGeneration/client';
-import { copyFile, writeFile } from 'fs/promises';
+import { copyFile, readFile, writeFile } from 'fs/promises';
 import { existsSync } from 'fs';
 
 const cwd = process.cwd();
@@ -15,6 +15,7 @@ class PreClientPlugin {
 			'PreClientPlugin',
 			async (_compilation, callback) => {
 				const routes = await getRoutes(cwd);
+				const mode = compiler.options.mode;
 
 				console.log(
 					chalk.hex('#0CCAE8').bold(`[client]`),
@@ -36,25 +37,25 @@ class PreClientPlugin {
 					chalk.hex('#4F58FF')('Writing Ahead.js files...'),
 				);
 
-				if (
-					!(await existsSync(
-						path.join(aheadDir, 'build', 'pre', 'client', 'router.tsx'),
-					))
-				)
-					await copyFile(
-						path.join(root, 'lib', 'client', 'router.tsx.txt'),
-						path.join(aheadDir, 'build', 'pre', 'client', 'router.tsx'),
-					);
+				await copyFile(
+					path.join(root, 'lib', 'client', 'router.tsx.txt'),
+					path.join(aheadDir, 'build', 'pre', 'client', 'router.tsx'),
+				);
 
-				if (
-					!(await existsSync(
-						path.join(aheadDir, 'build', 'pre', 'client', 'index.tsx'),
-					))
-				)
-					await copyFile(
-						path.join(root, 'lib', 'client', 'index.tsx.txt'),
-						path.join(aheadDir, 'build', 'pre', 'client', 'index.tsx'),
-					);
+				await writeFile(
+					path.join(aheadDir, 'build', 'pre', 'client', 'index.tsx'),
+					(
+						await readFile(
+							path.join(root, 'lib', 'client', 'index.tsx.txt'),
+							'utf-8',
+						)
+					).replace('AHEAD_ENV', `"${mode!}"`),
+				);
+
+				console.log(
+					chalk.hex('#0CCAE8').bold(`[client]`),
+					chalk.gray('Pre-Compilation finished!'),
+				);
 
 				callback();
 			},
