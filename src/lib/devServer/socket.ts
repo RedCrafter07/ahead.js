0012524e90d621214b8818fe10bfe6543078f897
RedCrafter07 <gh@r07.dev>
2023-07-10 14:20:00 +0200

    Handle last reload

--- src/lib/devServer/socket.ts
+++ src/lib/devServer/socket.ts
@@ -3,6 +3,7 @@ import { io, Socket } from 'socket.io-client';
 class DevSocketClient {
 	private client: Socket;
 	protected port: number;
+	protected lastReload: number = Date.now();
 
 	constructor(port: number) {
 		this.client = io(`http://localhost:${port}`, {
@@ -14,6 +15,15 @@ class DevSocketClient {
 
 		this.client.on('connect', () => {
 			console.log(`[ahead] Socket connected on port ${this.port}.`);
+		});
+
+		this.client.on('last', (lastReload: number) => {
+			if (lastReload > this.lastReload) {
+				console.log(`[ahead] Reloading...`);
+				window.location.reload();
+				return;
+			}
+			this.lastReload = lastReload;
 		});
 
 		this.client.on('reload', () => {
